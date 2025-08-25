0c1a80652b53eb6983c8791de10c1714c3cd9ed5
Divyansh Mishra <upkar8853@gmail.com>
2025-08-25 23:20:20 +0530

    added inverted pyramid

--- tuf_patterns/p7.java
+++ tuf_patterns/p7.java
@@ -1,30 +1,28 @@
 package tuf_patterns;
 
+// Following pattern
+//          *
+//         ***
+//        *****
+//       *******
+//      *********
+//     ***********
+//    *************
+//   ***************
+//  *****************
+// *******************
+
 public class p7 {
     public static void main(String[] args){
-        
-        for (int row = 0; row < 5; row++){
-            for (int spc = 0; spc < 5 - row - 1 ; spc++){
+        int lines = 10;
+        for (int row = 0; row < lines; row++){
+            for (int spc = 0; spc < lines - row - 1 ; spc++){
                 System.out.print(" ");
             }
-            int column = 0;
-            while (column<row){
-                for (int i = 0; i < column; i++){
-                    System.out.print("*");
-                }
-                column += 2;
-
+            for (int col = 0; col < 1 + row*2; col++){
+                System.out.print("*");
             }
-            // for (int col = 0; col < row; col++){
-            //     System.out.print("*");
-            // }
             System.out.println();
         }
     }
 }
-
-//     *
-//    *** 
-//   *****
-//  *******
-// *********
